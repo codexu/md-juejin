4c73b248d1470d691e31847517f89740f14a4309
codexu <461229187@qq.com>
2025-03-20 16:00:43 +0800

    feat: postinstall playwright

--- dist/cli.js
+++ dist/cli.js
@@ -5,20 +5,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const commander_1 = require("commander");
-const scraper_1 = require("./scraper");
 const chalk_1 = __importDefault(require("chalk"));
+const post_1 = require("./post");
+const saveMarkdown_1 = __importDefault(require("./saveMarkdown"));
+const saveImages_1 = __importDefault(require("./saveImages"));
 commander_1.program
     .name('md-juejin')
     .description('A CLI tool to scrape Juejin articles and convert them to Markdown')
     .version('1.0.0');
 commander_1.program
-    .command('scrape <url>')
+    .command('post <url>')
     .description('Scrape a Juejin article and convert it to Markdown')
     .option('-o, --output <dir>', 'Output directory', `${process.env.HOME}/Desktop`)
     .action(async (url, options) => {
     try {
         console.log(chalk_1.default.blue(`Starting to scrape article from ${url}`));
-        await (0, scraper_1.scrapeArticle)(url, options.output);
+        const articleInfo = await (0, post_1.scrapeArticle)(url);
+        const path = `${options.output}/${articleInfo.title}`;
+        await (0, saveMarkdown_1.default)(articleInfo.markdown, path, articleInfo.title || 'untitled');
+        await (0, saveImages_1.default)(articleInfo.images, path);
         console.log(chalk_1.default.green('Article scraped successfully!'));
     }
     catch (error) {
