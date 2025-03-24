1b76f513abff998b1e2980c38ae76f284070a6ae
codexu <461229187@qq.com>
2025-03-24 15:43:29 +0800

    perf: 👌通过接口获取文章列表

--- src/posts.ts
+++ src/posts.ts
@@ -1,41 +1,31 @@
-import { chromium, Page } from 'playwright'
+import axios from 'axios';
+import { JuejinRes } from './juejin';
+
+let isMore = true
 
 export async function scrapeArticles(url: string) {
-  const browser = await chromium.launch({ headless: true })
-  const context = await browser.newContext()
-  const page = await context.newPage()
-  await page.goto(url)
-  await page.waitForSelector('.post-list-header', { timeout: 10000 })
-  await autoScroll(page)
-  const articles = await page.$$('.entry-list>.item')
-  const articleList = []
-  for (const article of articles) {
-    const title = await article.$eval('.title', el => el.textContent?.trim().replace(/\s+/g, ''))
-    const url = await article.$eval('.title', el => el.getAttribute('href'))
-    articleList.push({ title, url: `https://juejin.cn${url}` })
+  const user_id = url.split('/')[4]
+  let cursor = 0;
+  const articleList: { title: string, url: string}[] = []
+  while (isMore) {
+    const res = await fetchPosts(user_id, cursor)
+    cursor += 10
+    articleList.push(...res)
+    if (!res.length) break
   }
-  await browser.close()
   return articleList
 }
 
-async function autoScroll(page: Page) {
-  await page.evaluate(async () => {
-    return new Promise<void>((resolve) => {
-      let totalHeight = 0;
-      const distance = 200;
-      let scrollAttempts = 0;
-      const maxScrollAttempts = 100;
-      let timer = setInterval(() => {
-        const scrollHeight = document.body.scrollHeight;
-        window.scrollBy(0, distance);
-        totalHeight += distance;
-        scrollAttempts++;
-        if (totalHeight >= scrollHeight - window.innerHeight || scrollAttempts >= maxScrollAttempts) {
-          clearInterval(timer);
-          resolve();
-        }
-      }, 500);
-    });
-  });
-  await page.waitForTimeout(1000);
+async function fetchPosts(user_id: string, cursor: number) {
+  const body = {
+    user_id,
+    sort_type: 2,
+    cursor: `${cursor}`,
+  }
+  const { data } = await axios.post<JuejinRes>('https://api.juejin.cn/content_api/v1/article/query_list', body)
+  isMore = data.has_more
+  return data.data.map((item) => ({
+    title: item.article_info.title,
+    url: `https://juejin.cn/post/${item.article_info.article_id}`
+  }))
 }
